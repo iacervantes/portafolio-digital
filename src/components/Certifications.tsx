88f9f0dffc350c8a701009719ccd0485dcb3beb1
iacervantes <cervantes_8709@hotmail.com>
2025-12-07 10:15:49 -0500

    Cambiar imagen de certificación de databricksv2

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -26,7 +26,7 @@ const certifications = [
     name: 'Databricks Certified Data Engineer',
     issuer: 'Databricks',
     date: 'Febrero 2024',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1HU497LXHjoNf9vj9oblPFk4ZBzChIrYr4Q&s',
+    logo: 'https://www.databricks.com/sites/default/files/2025-10/associate-badge-de.png',
     credential: 'https://www.credly.com/badges/example',
     color: 'from-red-500 to-orange-500',
   },
